8263d1a35412a9b05ba063dc5491f092d7c223ac
Aman Kumar Dubey <amankd91078@gmail.com>
2024-09-26 16:10:26 +0530

    Blog App using MERN

---
+++ index.js
@@ -0,0 +1,71 @@
+import express from "express";
+const app=express();
+
+
+app.use(express.json());
+let blogs=[]
+
+let user=[]
+app.post("/users",(req,res)=>{
+    const {name,email,password}=req.body;
+    try{
+        if(!name){
+           return res.status(300).json({
+                success:false,
+                message:"Please Enter your name "
+            })
+        }
+        if(!email){
+            return res.status(300).json({
+                success:false,
+                message:"Please your Enter your email "
+            })
+        }
+        if(!password){
+            return res.status(300).json({
+                success:false,
+                message:"Pleae Enter password"
+            });
+        }
+        user.push(req.body);
+        return res.status(200).json({
+            success:true,
+            message:"User Created Sucessfully"
+        })
+    } catch(error){
+
+    }
+})
+
+app.post("/blogs",(req,res)=>{
+    blogs.push({...req.body, id:blogs.length+1})
+    return res.json({message:"Successfully created blog"});
+})
+app.get("/blogs",(req,res)=>{
+    let publicBlog=blogs.filter(blog=>!blog.draft)
+    return res.json({publicBlog})
+})
+app.get("/blogs/:id",(req,res)=>{
+    const {id}=req.params
+    let SeachbyId=blogs.filter(blog=>blog.id==id)
+    return res.json({SeachbyId})
+
+})
+app.patch("/blogs/:id",(req,res)=>{
+    const {id}=req.params
+    // let index=blogs.findIndex(blog=>blog.id)
+    // blogs[index]={...blogs[index],...req.body}
+
+    let UpdatedBlogs=blogs.map((blog,index)=>blog.id==id?({...blogs[index],...req.body}):blog)
+    blogs=[...UpdatedBlogs]
+
+    return res.json({message:"Blog  Updated Successfully",UpdatedBlogs})
+
+})
+app.delete("/blogs/:id",(req,res)=>{
+
+})
+
+app.listen(3000,(req,res)=>{
+    console.log("App is running ");
+})
